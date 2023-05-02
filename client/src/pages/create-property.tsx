e16a2f1e4e7f8d68a107adbad76bc4a256166ca9
Vontell <sankarsh.rajeev@gmail.com>
2023-05-01 18:02:42 -0700

    Property add front end

--- client/src/pages/create-property.tsx
+++ client/src/pages/create-property.tsx
@@ -13,12 +13,25 @@ import Form from "components/common/Form";
 
 const CreateProperty = () => {
   const navigate = useNavigate();
-  const { data: identity } = useGetIdentity();
+  const { data: user } = useGetIdentity();
   const [propertyImage, setPropertyImage] = useState({ name: '', url: '' });
   const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
 
-  const handleImageChange = () => { };
-  const onFinishHandler = () => { };
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
+
+    reader(file).then((result: string) => setPropertyImage({ name: file?.name, url: result }));
+  };
+  const onFinishHandler =  async (data:FieldValues) => {
+    if(!propertyImage.name) 
+    return alert("Please Upload an Image");
+
+    await onFinish({...data,photo:propertyImage.url, email:user.email})
+   };
 
   return (
     <Form
